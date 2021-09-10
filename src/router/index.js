817b08921d74caffa0401d2cb8eebed7c7973680
aring <1303340995@qq.com>
2021-09-10 15:10:35 +0800

    fix: 修复切换路由页面未跳回顶部的问题

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,23 @@ export default new Router({
     {
       path: '/reaction-time',
       component: () => import('@/views/reaction-time/Index.vue')
+    },
+    {
+      path: '/number-memory',
+      component: () => import('@/views/number-memory/Index.vue')
+    },
+    {
+      path: '/chimp-test',
+      component: () => import('@/views/chimp-test/Index.vue')
+    },
+    {
+      path: '/typing',
+      component: () => import('@/views/typing/Index.vue')
     }
-  ]
+  ],
+  // 切换路由滚动到页面顶部
+  scrollBehavior() {
+    // return 期望滚动到哪个的位置
+    return { x: 0, y: 0 }
+  }
 })
--- src/views/index/config/data.js
+++ src/views/index/config/data.js
@@ -9,18 +9,18 @@ export const gameList = [
     icon: 'el-icon-tickets',
     title: '数字记忆',
     intro: '记住您看到的数字。',
-    path: ''
+    path: '/number-memory'
   },
   {
     icon: 'el-icon-menu',
     title: '黑猩猩测试',
     intro: '您比黑猩猩聪明吗？',
-    path: ''
+    path: '/chimp-test'
   },
   {
     icon: 'el-icon-s-platform',
     title: '打字测试',
     intro: '您每分钟可以打多少字？',
-    path: ''
+    path: '/typing'
   }
 ]
